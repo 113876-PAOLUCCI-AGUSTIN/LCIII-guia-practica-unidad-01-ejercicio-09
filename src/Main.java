23e14a2bda927444cb30fe3a09b512c424729c88
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-23 20:49:49 -0300

    Problema planteado.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+/*
+* 9. Ingresar un número n y a continuación n números positivos.
+* Informar la cantidad de mayores a 5 que se hayan ingresado.
+*/
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
